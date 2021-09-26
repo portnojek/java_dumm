b18b1845449d558085266fa3bbcbd1ccf44d69b6
Bartek <Bartek@MacBook-Pro-Bartek.local>
2021-09-26 10:06:57 +0200

    update

--- src/first_class_z400.java
+++ src/first_class_z400.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class first_class_z400 {
 
     public static void main(String[] args){
 
         System.out.println("Witaj świecie! ");
-
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.printf("Witaj "+name);
 
     }
 
